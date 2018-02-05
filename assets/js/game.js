3ab41b8d5999b151cb873c6ba1a8ceff8bd58ef8
SiriusScaper <siriusscaper@gmail.com>
2018-02-05 14:02:39 -0800

    Add background, some css to arrange elements and some js

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,6 +6,15 @@ let score = 0;
 
 let randWord = [];
 
+
+// Get random word function
+
+
+
+
+
+
+
 // Display title of game
 // Display high scores
     //input 
@@ -29,22 +38,23 @@ let randWord = [];
 var canvas;
 var cntx;
 
-
-window.onLoad = function draw() {
-    console.log('Hello World')
-    newFunction();
+function draw() {
+    canvas = document.getElementById('#gameCanvas');
+    cntx = canvas.getContext('2d', { alpha: false });
 };
 
+window.onload = draw();
 
-
-function draw() {
-    canvas = document.getElementById('gameCanvas');
-    cntx = canvas.getContext('2d', { alpha: false });
-    cntx.fillStyle = 'black';
-    cntx.fillRect(0, 0, canvas.width, canvas.height);
-}
 // function testInput(event) {
 //     var value = String.fromCharCode(event.which);
 //     var pattern = new RegExp(/[a-zåäö ]/i);
 //     return pattern.test(value);
 //  }
+
+
+
+
+
+
+
+
