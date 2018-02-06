cdaa9931e6f0c1800943ac92810c15c2158caf44
SiriusScaper <siriusscaper@gmail.com>
2018-02-06 01:27:13 -0800

    added words.js

---
+++ assets/js/words.js
@@ -0,0 +1,64 @@
+
+/*
+    deathmatch
+    dungeon crawl
+
+    expansion pack
+    experience point
+
+    fatality
+    fighting game
+    first-person shooter
+    flight sim
+    fog of war
+
+    game mechanics
+    game over
+    gameplay
+    gamer
+    griefer
+    grinding
+
+    hack and slash
+    handheld game console
+    hitbox
+
+    indie game
+    item
+
+    lag
+    level
+    light gun shooter
+    line of sight
+    looting
+
+    metroidvania
+    minigame
+    mob
+
+    non-player character
+
+    paper doll
+    permadeath
+    Platform game
+    player character
+    player versus environment
+    player versus player
+    power-up
+
+    quest
+
+    random encounter
+    real-time strategy
+    roguelike
+    role-playing-game
+    saved game
+    shooter game
+    side-scrolling
+    spawn point
+    spawning
+    special attacks 
+    third-person shooter
+    arcade cabinet
+    zone
+]; */
